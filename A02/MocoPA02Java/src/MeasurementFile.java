36dafddff37e3717928b773386e0f22ef3296ed4
dbonillao <delcybonilla91@gmail.com>
2016-06-16 15:48:29 +0200

    Deletes old Http and Ping excel files

--- A02/MocoPA02Java/src/MeasurementFile.java
+++ A02/MocoPA02Java/src/MeasurementFile.java
@@ -137,54 +137,55 @@ public class MeasurementFile {
 	public void writeHttpExcel(ArrayList<HttpObject> httpArray, String excelFilePath){
 		// open stream
 		try {
-			File excelFile = new File(excelFilePath);
-			if (!excelFile.exists()) {
-				// create File
-				XSSFWorkbook wb = new XSSFWorkbook();
-				// new Sheets
-				XSSFSheet sheet1 = wb.createSheet("Sheet1");
-				// create Title cells
-				Cell cell = null;
-				XSSFRow sheetrow;
-				sheetrow = sheet1.createRow(0);
-				cell = sheetrow.getCell(0);
-				if (cell == null) {
-					cell = sheetrow.createCell(0);
-					cell.setCellValue("Timestamp");
-					cell = sheetrow.createCell(1);
-					cell.setCellValue("Bytes");
-					cell = sheetrow.createCell(2);
-					cell.setCellValue("Time");
-				}
-				FileOutputStream fileOut = new FileOutputStream(excelFile);
-				wb.write(fileOut);
-				fileOut.close();
+			//deleting old File to avoid duplicates
+			File oldExcelFile = new File(excelFilePath);
+			if (oldExcelFile.exists()) {
+				oldExcelFile.delete();
 			}
+			File excelFile = new File(excelFilePath);
+			// create File
+			XSSFWorkbook wb = new XSSFWorkbook();
+			// new Sheets
+			XSSFSheet sheet1 = wb.createSheet("Sheet1");
+			// create Title cells
+			Cell cell = null;
+			XSSFRow sheetrow;
+			sheetrow = sheet1.createRow(0);
+			cell = sheetrow.getCell(0);
+			if (cell == null) {
+				cell = sheetrow.createCell(0);
+				cell.setCellValue("Timestamp");
+				cell = sheetrow.createCell(1);
+				cell.setCellValue("Bytes");
+				cell = sheetrow.createCell(2);
+				cell.setCellValue("Time");
+			}
+			FileOutputStream fileOut = new FileOutputStream(excelFile);
+			wb.write(fileOut);
+			fileOut.close();
 			FileInputStream infile = new FileInputStream(excelFile);
 			XSSFWorkbook workbook = new XSSFWorkbook(infile);
 			// getsheet
 			XSSFSheet sheet = workbook.getSheetAt(0);
 			
 			for(HttpObject httpObj: httpArray){
-				//if(!columnContains(sheet, httpObj.getTimestamp())){
-					// get number or row
-					int rowNumSheet = sheet.getLastRowNum() + 1;
-					Cell cell = null;
-					XSSFRow sheetrow= sheet.getRow(rowNumSheet);
-					if (sheetrow == null) {
-						sheetrow = sheet.createRow(rowNumSheet);
-					}
-					// update the value of cell
-					cell = sheetrow.getCell(0);
-					if (cell == null) {
-						cell = sheetrow.createCell(0);
-						cell.setCellValue(httpObj.getTimestamp());
-						cell = sheetrow.createCell(1);
-						cell.setCellValue(httpObj.getBytes());
-						cell = sheetrow.createCell(2);
-						cell.setCellValue(httpObj.getTime());
-					}
-				//}
+				// get number or row
+				int rowNumSheet = sheet.getLastRowNum() + 1;
+				Cell newCell = null;
+				XSSFRow newRow = sheet.getRow(rowNumSheet);
+				if (newRow == null) {
+					newRow = sheet.createRow(rowNumSheet);
+				}
+				// update the value of cell
+				newCell = newRow.getCell(0);
+				if (newCell == null) {
+					newCell = newRow.createCell(0);
+					newCell.setCellValue(httpObj.getTimestamp());
+					newCell = newRow.createCell(1);
+					newCell.setCellValue(httpObj.getBytes());
+					newCell = newRow.createCell(2);
+					newCell.setCellValue(httpObj.getTime());
+				}
 			}
 			// =====================================================
 			// outputstream, close stream.
@@ -203,88 +204,77 @@ public class MeasurementFile {
 	
 	public void writePingExcel(ArrayList<PingObject> pingArray, String excelFilePath){
 		// open stream
-				try {
-					File excelFile = new File(excelFilePath);
-					if (!excelFile.exists()) {
-						// create File
-						XSSFWorkbook wb = new XSSFWorkbook();
-						// new Sheets
-						XSSFSheet sheet1 = wb.createSheet("Sheet1");
-						// create Title cells
-						Cell cell = null;
-						XSSFRow sheetrow;
-						sheetrow = sheet1.createRow(0);
-						cell = sheetrow.getCell(0);
-						if (cell == null) {
-							cell = sheetrow.createCell(0);
-							cell.setCellValue("Timestamp");
-							cell = sheetrow.createCell(1);
-							cell.setCellValue("Packetloss");
-							cell = sheetrow.createCell(2);
-							cell.setCellValue("RTTmin");
-							cell = sheetrow.createCell(3);
-							cell.setCellValue("RTTavg");
-							cell = sheetrow.createCell(4);
-							cell.setCellValue("RTTmax");
-						}
-						FileOutputStream fileOut = new FileOutputStream(excelFile);
-						wb.write(fileOut);
-						fileOut.close();
-					}
-					FileInputStream infile = new FileInputStream(excelFile);
-					XSSFWorkbook workbook = new XSSFWorkbook(infile);
-					// getsheet
-					XSSFSheet sheet = workbook.getSheetAt(0);
-					
-					for(PingObject pingObj: pingArray){
-						//if(!columnContains(sheet, pingObj.getTimestamp())){
-							// get number or row
-							int rowNumSheet = sheet.getLastRowNum() + 1;
-							Cell cell = null;
-							XSSFRow sheetrow= sheet.getRow(rowNumSheet);
-							if (sheetrow == null) {
-								sheetrow = sheet.createRow(rowNumSheet);
-							}
-							// update the value of cell
-							cell = sheetrow.getCell(0);
-							if (cell == null) {
-								cell = sheetrow.createCell(0);
-								cell.setCellValue(pingObj.getTimestamp());
-								cell = sheetrow.createCell(1);
-								cell.setCellValue(pingObj.getPacketloss());
-								cell = sheetrow.createCell(2);
-								cell.setCellValue(pingObj.getRTTmin());
-								cell = sheetrow.createCell(3);
-								cell.setCellValue(pingObj.getRTTavg());
-								cell = sheetrow.createCell(4);
-								cell.setCellValue(pingObj.getRTTmax());
-							}
-						//}
-					}
-					// =====================================================
-					// outputstream, close stream.
-					infile.close();
-					FileOutputStream outFile = new FileOutputStream(excelFile);
-					workbook.write(outFile);
-					outFile.close();
-					// System.out.println("Success updated..");
-
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	}
-	
-	public Boolean columnContains(XSSFSheet sheet, long timestamp){
-		int lastRow = sheet.getLastRowNum();
-		for(int i = 1; i <= lastRow; i++){
-			Row row = sheet.getRow(i);
-			Cell cell = row.getCell(0);
-			if(timestamp == cell.getNumericCellValue()){
-				return true;
+		try {
+			// deleting old File to avoid duplicates
+			File oldExcelFile = new File(excelFilePath);
+			if (oldExcelFile.exists()) {
+				oldExcelFile.delete();
 			}
+			File excelFile = new File(excelFilePath);
+			// create File
+			XSSFWorkbook wb = new XSSFWorkbook();
+			// new Sheets
+			XSSFSheet sheet1 = wb.createSheet("Sheet1");
+			// create Title cells
+			Cell cell = null;
+			XSSFRow sheetrow;
+			sheetrow = sheet1.createRow(0);
+			cell = sheetrow.getCell(0);
+			if (cell == null) {
+				cell = sheetrow.createCell(0);
+				cell.setCellValue("Timestamp");
+				cell = sheetrow.createCell(1);
+				cell.setCellValue("Packetloss");
+				cell = sheetrow.createCell(2);
+				cell.setCellValue("RTTmin");
+				cell = sheetrow.createCell(3);
+				cell.setCellValue("RTTavg");
+				cell = sheetrow.createCell(4);
+				cell.setCellValue("RTTmax");
+			}
+			FileOutputStream fileOut = new FileOutputStream(excelFile);
+			wb.write(fileOut);
+			fileOut.close();
+			FileInputStream infile = new FileInputStream(excelFile);
+			XSSFWorkbook workbook = new XSSFWorkbook(infile);
+			// getsheet
+			XSSFSheet sheet = workbook.getSheetAt(0);
+
+			for (PingObject pingObj : pingArray) {
+				// get number or row
+				int rowNumSheet = sheet.getLastRowNum() + 1;
+				Cell newCell = null;
+				XSSFRow Row = sheet.getRow(rowNumSheet);
+				if (Row == null) {
+					Row = sheet.createRow(rowNumSheet);
+				}
+				// update the value of cell
+				newCell = Row.getCell(0);
+				if (newCell == null) {
+					newCell = Row.createCell(0);
+					newCell.setCellValue(pingObj.getTimestamp());
+					newCell = Row.createCell(1);
+					newCell.setCellValue(pingObj.getPacketloss());
+					newCell = Row.createCell(2);
+					newCell.setCellValue(pingObj.getRTTmin());
+					newCell = Row.createCell(3);
+					newCell.setCellValue(pingObj.getRTTavg());
+					newCell = Row.createCell(4);
+					newCell.setCellValue(pingObj.getRTTmax());
+				}
+			}
+			// =====================================================
+			// outputstream, close stream.
+			infile.close();
+			FileOutputStream outFile = new FileOutputStream(excelFile);
+			workbook.write(outFile);
+			outFile.close();
+			// System.out.println("Success updated..");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		return false;
 	}
 }
